3cff1f952b26b94de0574d719e95af51396f27c2
Ivan Stupin <gognit65@gmail.com>
2023-11-27 20:27:25 +0500

    HW 2 Done

--- Arrays/ArraysHWzadanie2.cs
+++ Arrays/ArraysHWzadanie2.cs
@@ -15,8 +15,8 @@ namespace Arrays // Note: actual namespace depends on the project name.
             {
                 Console.Clear();
 
-                int[] array = { 1, 2, 5, 3, 4, 5, 3, 5 }; //создаём массив
-                Console.WriteLine("Введите целое число для его удаления из массива [1, 2, 5, 3, 4, 5, 3, 5]: \n");
+                int[] array = { 1, 2, 6, 2, 3, 3, 0, 3, 4 }; //создаём массив
+                Console.WriteLine("Введите целое число для его удаления из массива [1, 2, 6, 2, 3, 3, 0, 3, 4]: \n");
                 int usersNumbersToDelete = Convert.ToInt32(Console.ReadLine());
 
                 int indexUsersNumbersToDelete = 0;  // переменная для хранения индекса в массиве введённого числа
@@ -40,7 +40,7 @@ namespace Arrays // Note: actual namespace depends on the project name.
                 }
                 while (countNotDistinctNumbersInArrays >=1) //цикл для обработки повторяющихся значений в массиве
                 {
-                    int[] newArray = new int[array.GetLength(0) - 1]; //новый массив с уменьшением размера на 1 в каждой итерации цикла
+                    int[] newArray = new int[array.GetLength(0) - 1]; //новый массив с уменьшением размера на 1 в каждой итерации цикла while
                     
                     for (int i = 0; i < array.GetLength(0); i++) //цикл для поиска индекса
                     {
@@ -52,15 +52,15 @@ namespace Arrays // Note: actual namespace depends on the project name.
                     
                     for (int i = 0; i < indexUsersNumbersToDelete; i++) // цикл 1,вносим значения в новый массив до введённного символа
                     {
-                        newArray[i] = array[i];
+                        newArray[i] = array[i]; // в новый массив вносим значения из старого
                     }
 
-                    for (int i = indexUsersNumbersToDelete + 1; i <= array.GetUpperBound(0); i++) // цикл 2,вносим значения в новый массив после введённого символа
+                    for (int i = indexUsersNumbersToDelete + 1; i <= array.GetUpperBound(0); i++) // цикл 2,вносим значения в новый массив после введённого символа + 1, тем самым пропуская введённое значение
                     {
-                        newArray[i - 1] = array[i];
+                        newArray[i - 1] = array[i]; 
                     }
 
-                    countNotDistinctNumbersInArrays -= 1; // уменьшаем счётчик повторяющихся символов в массиве
+                    countNotDistinctNumbersInArrays -= 1; // уменьшаем счётчик повторяющихся символов в массиве на 1 в кажой итерации
 
                     array = newArray;  // присваиваем в старый массив новый
                 }
@@ -70,7 +70,7 @@ namespace Arrays // Note: actual namespace depends on the project name.
                     Console.WriteLine(array[i]);
                 }
                 
-                Console.ReadLine(); // ожидание вввода с  консоли
+                Console.ReadLine(); // ожидание ввода с  консоли
             }
         }
     }
