832f191e891255b77a8414ca04ab335ee8673420
Ivan Stupin <gognit65@gmail.com>
2024-01-08 19:37:42 +0500

    Classes HW

---
+++ Classes_HW2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Classes_HW2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditCard card1 = new CreditCard(1, 500);
+            CreditCard card2 = new CreditCard(2, 375);
+            CreditCard card3 = new CreditCard(3, 125);
+
+            card1.InfoCreditCard();
+            card1.DepositeToBill(220);
+            card1.InfoCreditCard();
+
+            card2.InfoCreditCard();
+            card2.DepositeToBill(25);
+            card2.InfoCreditCard();
+
+            card3.InfoCreditCard();
+            card3.DepositeWithdraw(25);
+            card3.InfoCreditCard();
+        }
+    }
+}
